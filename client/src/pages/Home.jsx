7b4c47a61261e012e4333968b9e48154979430c5
unknown <zekuzakeane@gmail.com>
2024-02-07 20:26:17 -0500

    Redid Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,24 +3,34 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <div>
-        <p>Hello! Please select from one of the choices above.</p>
-      </div>
-      <div>
+      <header>
+        <h3><b>4EverMemories</b></h3>
+      </header>
+      <main>
+        <img src='https://img1.wsimg.com/isteam/stock/11270/:/rs=w:1280'></img>
         <div>
-          <h1>Capturing Life's Moments</h1>
-          <span></span>
+          <h4>Capturing Life's Moments</h4>
+          <h2>Professional photography for weddings, families and events</h2>
+          <button type='button'>Book a Session</button>
         </div>
-      </div>
-      <div>
         <div>
-          <p>
-            <span>
-              Professional photography for weddings, families, and events
-            </span>
-          </p>
+          <h3>Capturing Life: 4EverMemories Photography Gallery</h3>
+          <div>
+
+          </div>
         </div>
-      </div>
+        <div>
+          <h2>Keep Up to Date!</h2>
+          <h4>Sign up to hear about newly released prints and upcoming events!</h4>
+          <form>
+            <input type='email' placeholder='Email Address'></input>
+            <button type='button'>Sign Up</button>
+          </form>
+        </div>
+      </main>
+      <footer>
+        <h5>Copyright © 2024 4EverMemories - All Rights Reserved.</h5>
+      </footer>
     </div>
   )
 }
