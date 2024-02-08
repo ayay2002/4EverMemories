908953b661f77b37f70a886a6d47078ba6e912f0
NoFox <nfox598625@gmail.com>
2024-02-08 18:24:11 -0500

    changed .env to .env.example and created albums

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,14 +5,21 @@ type Photographer {
     email: String
     image: String
 }
+
 type User {
     userId: ID!
     name: String!
     email: String
     photographers: [Photographer]
 }
+
 type Query {
 photographer(photographerId: ID): Photographer 
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+
 }
 `;
 
