4b7949fd3aa5259b7f57e34da9930d8392a8297c
NoFox <nfox598625@gmail.com>
2024-02-07 19:06:19 -0500

    added syntax for apolloServer

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: String,
-    email: {
-        type: String,
-        uniique: true,
-    },
-    password: String
-})
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+});
 
-const UserModel = mongoose.model('User', userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
-module.exports = UserModel
+module.exports = UserModel;
--- server/server.js
+++ server/server.js
@@ -1,26 +1,31 @@
 const express = require("express");
-
-const app = express();
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
+require("dotenv").config({ path: "./.env" });
 
 const cors = require("cors");
+app.use(require("./routes/record"));
 
-require("dotenv").config({ path: "./.env" });
+// Get MongoDB driver connection
+const dbo = require("./db/conn");
+const { typeDefs, resolvers } = require("./schemas");
+
 const port = process.env.PORT || 5000;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use(require("./routes/record"));
-
-// Get MongoDB driver connection
-const dbo = require("./db/conn");
- 
 app.listen(port, () => {
   // Perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
   });
   console.log(`Server is running on port: ${port}`);
 });
