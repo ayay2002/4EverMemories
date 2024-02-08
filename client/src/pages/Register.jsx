719eeb7d2d63097ce1603ff3e30cd80687074bda
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-02-07 18:43:36 -0600

    update to register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,23 +5,30 @@ import { useNavigate } from "react-router-dom";
 
 export default function Register() {
     const navigate = useNavigate()
-    const [data, setData] = useState({
+    const [data1, setData1] = useState({
         name: '',
         email: '',
         password: '',
     })
 
+    const [ data2, setData2] = useState({
+        name: '',
+        email: '',
+        password: '',
+    })
+
+
     const registerUser = async (e) => {
         e.preventDefault()
-        const { name, email, password } = data
+        const { name, email, password } = data1
         try {
-            const { data } = await axios.post('/register', {
+            const { data1 } = await axios.post('/register', {
                 name, email, password
             })
-            if (data.error) {
+            if (data1.error) {
                 toast.error(data.error)
             } else {
-                setData({})
+                setData1({})
                 toast.success('Login successful, Welcome!')
                 navigate('/login')
             }
@@ -32,15 +39,15 @@ export default function Register() {
 
     const registerPhotographer = async (e) => {
         e.preventDefault()
-        const { name, email, password } = data
+        const { name, email, password } = data2
         try {
-            const { data } = await axios.post('/register', {
+            const { data2 } = await axios.post('/register', {
                 name, email, password
             })
-            if (data.error) {
+            if (data2.error) {
                 toast.error(data.error)
             } else {
-                setData({})
+                setData2({})
                 toast.success('Login successful, Welcome!')
                 navigate('/login')
             }
@@ -57,11 +64,11 @@ export default function Register() {
             <h3>User Register</h3>
             <form onSubmit={registerUser}>
                 <label>Name</label>
-                <input type="text" placeholder='enter name...' value={data.name} onChange={(e) => setData({ ...data, name: e.target.value })} />
+                <input type="text" placeholder='enter name...' value={data1.name} onChange={(e) => setData1({ ...data1, name: e.target.value })} />
                 <label>Email</label>
-                <input type="email" placeholder='enter email...' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+                <input type="email" placeholder='enter email...' value={data1.email} onChange={(e) => setData1({ ...data1, email: e.target.value })} />
                 <label>Password</label>
-                <input type="password" placeholder='enter password...' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+                <input type="password" placeholder='enter password...' value={data1.password} onChange={(e) => setData1({ ...data1, password: e.target.value })} />
                 <button type='submit'>Submit</button>
             </form>
         </div>
@@ -69,11 +76,11 @@ export default function Register() {
             <h3>Photographer Register</h3>
             <form onSubmit={registerPhotographer}>
                 <label>Name</label>
-                <input type="text" placeholder='enter name...' value={data.name} onChange={(e) =>   setData({ ...data, name: e.target.value })} />
+                <input type="text" placeholder='enter name...' value={data2.name} onChange={(e) =>   setData2({ ...data2, name: e.target.value })} />
                 <label>Email</label>
-                <input type="email" placeholder='enter email...' value={data.email} onChange={(e) =>    setData({ ...data, email: e.target.value })} />
+                <input type="email" placeholder='enter email...' value={data2.email} onChange={(e) =>    setData2({ ...data2, email: e.target.value })} />
                 <label>Password</label>
-                <input type="password" placeholder='enter password...' value={data.password} onChange=  {(e) => setData({ ...data, password: e.target.value })} />
+                <input type="password" placeholder='enter password...' value={data2.password} onChange=  {(e) => setData2({ ...data2, password: e.target.value })} />
                 <button type='submit'>Submit</button>
             </form>
         </div>
