63fcbc6d4fe5f9675b18ea1758de7c878c500907
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-02-09 08:59:50 -0600

    update to formatting in register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -11,7 +11,7 @@ export default function Register() {
         password: '',
     })
 
-    const [ data2, setData2] = useState({
+    const [data2, setData2] = useState({
         name: '',
         email: '',
         password: '',
@@ -27,14 +27,14 @@ export default function Register() {
                 name, email, password
             })
             if (data1.error) {
-            if (data.error) {
-                toast.error(data.error)
-            } else {
-                setData1({})
-                toast.success('Login successful, Welcome!')
-                navigate('/login')
+                if (data.error) {
+                    toast.error(data.error)
+                } else {
+                    setData1({})
+                    toast.success('Login successful, Welcome!')
+                    navigate('/login')
+                }
             }
-        }
         } catch (error) {
             console.log(error)
         }
@@ -59,34 +59,34 @@ export default function Register() {
         }
     }
 
-    
+
 
     return (
         <div>
-        <div className="user-form">
-            <h3>User Register</h3>
-            <form onSubmit={registerUser}>
-                <label>Name</label>
-                <input type="text" placeholder='enter name...' value={data1.name} onChange={(e) => setData1({ ...data1, name: e.target.value })} />
-                <label>Email</label>
-                <input type="email" placeholder='enter email...' value={data1.email} onChange={(e) => setData1({ ...data1, email: e.target.value })} />
-                <label>Password</label>
-                <input type="password" placeholder='enter password...' value={data1.password} onChange={(e) => setData1({ ...data1, password: e.target.value })} />
-                <button type='submit'>Submit</button>
-            </form>
-        </div>
-        <div className="photographer-form">
-            <h3>Photographer Register</h3>
-            <form onSubmit={registerPhotographer}>
-                <label>Name</label>
-                <input type="text" placeholder='enter name...' value={data2.name} onChange={(e) =>   setData2({ ...data2, name: e.target.value })} />
-                <label>Email</label>
-                <input type="email" placeholder='enter email...' value={data2.email} onChange={(e) =>    setData2({ ...data2, email: e.target.value })} />
-                <label>Password</label>
-                <input type="password" placeholder='enter password...' value={data2.password} onChange=  {(e) => setData2({ ...data2, password: e.target.value })} />
-                <button type='submit'>Submit</button>
-            </form>
-        </div>
+            <div className="user-form">
+                <h3>User Register</h3>
+                <form onSubmit={registerUser}>
+                    <label>Name</label>
+                    <input type="text" placeholder='enter name...' value={data1.name} onChange={(e) => setData1({ ...data1, name: e.target.value })} />
+                    <label>Email</label>
+                    <input type="email" placeholder='enter email...' value={data1.email} onChange={(e) => setData1({ ...data1, email: e.target.value })} />
+                    <label>Password</label>
+                    <input type="password" placeholder='enter password...' value={data1.password} onChange={(e) => setData1({ ...data1, password: e.target.value })} />
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+            <div className="photographer-form">
+                <h3>Photographer Register</h3>
+                <form onSubmit={registerPhotographer}>
+                    <label>Name</label>
+                    <input type="text" placeholder='enter name...' value={data2.name} onChange={(e) => setData2({ ...data2, name: e.target.value })} />
+                    <label>Email</label>
+                    <input type="email" placeholder='enter email...' value={data2.email} onChange={(e) => setData2({ ...data2, email: e.target.value })} />
+                    <label>Password</label>
+                    <input type="password" placeholder='enter password...' value={data2.password} onChange={(e) => setData2({ ...data2, password: e.target.value })} />
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
         </div>
     )
-    }
+}
